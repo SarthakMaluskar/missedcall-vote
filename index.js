a93e815b70be8b908e959a26febe6ef01c13bc8d
SarthakMaluskar <sarthak.malu1@gmail.com>
2026-05-09 15:18:26 +0530

    fixed some issues

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
+
 app.post("/webhook/call", (req, res) => {
 
-    if(!req.body){
-        console.log("got nothing");
-       return res.sendStatus(401);
-        
-    }
-    console.log("BODY:", req.body);
+    console.log("FULL QUERY:", req.query);
+
+    console.log("Caller Number:", req.query.CallFrom);
 
     return res.sendStatus(200);
-    
 });
 
 app.listen(PORT, () => {
-    console.log("Server running");
+    console.log(`Server running on ${PORT}`);
 });
