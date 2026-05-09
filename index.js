a8d6e42083c724bc07f3ff194fc0d4661739f77d
SarthakMaluskar <sarthak.malu1@gmail.com>
2026-05-09 15:28:26 +0530

    checking both req.query and req.body

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ const PORT = process.env.PORT || 3000;
 
 app.post("/webhook/call", (req, res) => {
 
-    console.log("FULL QUERY:", req.query);
+    console.log("BODY:", req.body);
 
-    console.log("Caller Number:", req.query.CallFrom);
+    console.log("QUERY:", req.query);
 
-    return res.sendStatus(200);
+    console.log("CallFrom BODY:", req.body.CallFrom);
+
+    console.log("CallFrom QUERY:", req.query.CallFrom);
+
+    return res.status(200).send("OK");
 });
 
 app.get("/webhook/call", (req, res) => {
