1ecbdfe25f8829c2a658eaa9b2a80b9a0a9ba7a8
SarthakMaluskar <sarthak.malu1@gmail.com>
2026-05-09 15:15:30 +0530

    fixed the port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+const PORT = process.env.PORT || 3000;
 app.post("/webhook/call", (req, res) => {
 
     if(!req.body){
@@ -18,6 +18,6 @@ app.post("/webhook/call", (req, res) => {
     
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server running");
 });
